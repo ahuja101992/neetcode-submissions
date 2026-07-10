d404cf6ef0a8774f1a5c8ed087c66578c34aff0d
Akshit Ahuja <ahuja101992@users.noreply.github.com>
2026-07-10 14:09:17 -0500

    Add: count-paths - submission-1

---
+++ Data Structures & Algorithms/count-paths/submission-1.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[] arr = new int[n];
+        Arrays.fill(arr,1);
+        for(int i = 1;i<m;i++){
+            for (int j=1;j<n;j++){
+                
+                arr[j] = arr[j] + arr[j-1];
+                
+            }
+        }
+        return arr[n-1];
+    }
+}
