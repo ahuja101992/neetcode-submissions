c66e911393cdc17754add7688c6c7c27ff4bf826
Akshit Ahuja <ahuja101992@users.noreply.github.com>
2026-07-16 16:14:19 -0500

    Add: foreign-dictionary - submission-6

---
+++ Data Structures & Algorithms/foreign-dictionary/submission-6.java
@@ -0,0 +1,53 @@
+class Solution {
+    public String foreignDictionary(String[] words) {
+      Map<Character, Integer> indegree = new HashMap<>();
+      Map<Character, Set<Character>> adj = new HashMap<>();
+      for(String word: words){
+        for(char ch: word.toCharArray()){
+            indegree.putIfAbsent(ch, 0);
+            adj.putIfAbsent(ch, new HashSet<>());
+        }
+      }
+      for (int i = 0; i < words.length - 1; i++) {
+        String w1 = words[i];
+        String w2 = words[i+1];
+        int minLen = Math.min(w1.length(), w2.length());
+         if (w1.length() > w2.length() &&
+                w1.substring(0, minLen).equals(w2.substring(0, minLen))) {
+                return "";
+            }
+        for( int j=0;j<Math.min(w1.length(), w2.length());j++){
+            if(w1.charAt(j)!= w2.charAt(j)){
+                if(!adj.get(w1.charAt(j)).contains(w2.charAt(j))){
+                    indegree.put(w2.charAt(j), indegree.get(w2.charAt(j))+1);
+                    adj.get(w1.charAt(j)).add(w2.charAt(j));
+                }
+                break ;
+            }
+        }
+      }
+
+      Queue<Character> queue = new LinkedList<>();
+      for(char ch: indegree.keySet()){
+        if(indegree.get(ch)==0){
+            queue.offer(ch);
+        }
+      }
+      StringBuilder sb = new StringBuilder();
+      while(!queue.isEmpty()){
+        char curr = queue.poll();
+        sb.append(curr);
+        Set<Character> list = adj.get(curr);
+        for(char ch : list){
+            indegree.put(ch, indegree.get(ch)-1);
+            if(indegree.get(ch) ==0){
+                queue.offer(ch);
+            }
+        }
+      }
+      if(sb.length()!= indegree.size()){
+        return "";
+      }
+      return sb.toString();
+    }
+}
